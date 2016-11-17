748f952500f7002645827e759323738641109f2f
Ben Pig Chu <benpichu@gmail.com>
2016-11-17 14:06:13 +0800

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:style="{'background-color':$store.state.backgroundColor,color:$store.getters.textColor,'transition-property':'background,color','transition-duration':'200ms'}">
 	  <head-bar></head-bar>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view id="view"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -14,4 +16,8 @@ export default {
 </script>
 
 <style>
+#app{position:fixed;width:100%;height:100%;margin:0;padding:0;left:0;top:0}
+.fade-enter-active, .fade-leave-active {transition: opacity 200ms}
+.fade-enter, .fade-leave-active {opacity: 0}
+#view{position:absolute}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,26 +4,52 @@ import VueRouter from 'vue-router'
 Vue.use(Vuex)
 Vue.use(VueRouter)
 
+import Color from 'color'
+
 import App from './App.vue'
 
 const router = new VueRouter({
   mode: 'hash',
   routes: [
-    { path: '/', name: 'home', component: {template:"<div>home</div>"} },
-    { path: '/color-:hex', name: 'color', component: {template:"<div>color:#{{$route.params.hex}}</div>"} }
+    {
+      path: '/',
+      name: 'home',
+      component: { template: "<div>home</div>" }
+    },
+    {
+      path: '/color-:hex',
+      name: 'color',
+      component: { template: "<div>color:#{{$route.params.hex}}</div>" }
+    }
   ]
 })
 
 const store = new Vuex.Store({
   state: {
-    view: 'home'
+    view: 'home',
+    backgroundColor: '#ff7f00',
+    params:{}
+  },
+  getters: {
+    textColor: (state) => new Color(state.backgroundColor).luminosity() <= 0.5 ? "#FFFFFF" : "#000000"
   },
   mutations: {
-
+    replaceBackgroundColor(state, color) {
+      state.backgroundColor = color;
+    },
+    navigate(state, route) {
+      state.view=route.name
+      state.params=route.params
+    }
   }
 })
 
-const app=new Vue({
+router.beforeEach((to, from, next) => {
+  store.commit('navigate',to)
+  next()
+})
+
+const app = new Vue({
   el: '#app',
   router,
   store,
