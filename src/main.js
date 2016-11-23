0ac612f0d693cd4275495a9de8720057ba31f6e2
Ben Pig Chu <benpichu@gmail.com>
2016-11-23 21:08:54 +0800

    route stuff

--- src/main.js
+++ src/main.js
@@ -33,16 +33,37 @@ const router = new VueRouter({
 
 const store = new Vuex.Store({
   state: {
+    colorSelected: false,
     selectedColor: "#000000",
   },
   getters: {
-    textColor: (state) => new Color(state.backgroundColor).luminosity() <= 0.5 ? "#FFFFFF" : "#000000"
+    textColor: (state) => new Color(state.selectedColor).luminosity() <= 0.5 ? "#FFFFFF" : "#000000"
   },
   mutations: {
     selectColor(state, color) {
       state.selectedColor = color;
+    },
+    toggleSelectStatus(state, selected) {
+      state.colorSelected = selected;
+    }
+  },
+  actions: {
+    navigation(context,route) {
+      context.commit("toggleSelectStatus",route.name=="color")
+      if(route.name=="color"){
+        if(route.params.hex in list){
+          new Color("#"+route.params.hex)
+          context.commit("selectColor","#"+route.params.hex)
+        }else{
+          router.replace("/"+context.state.selectedColor.slice(1))
+        }
+      }
     }
   }
+})
+
+router.afterEach((to, from) => {
+  store.dispatch("navigation",to)
 })
 
 const app = new Vue({
