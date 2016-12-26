e7bf7bfe8fc93690a84fa60edb5d3bef2fa3bb63
Ben Pig Chu <benpichu@gmail.com>
2016-12-26 21:15:36 +0800

    general layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="app" :style="{'background-color':$store.state.selectedColor,color:$store.getters.textColor}">
 		<color-info class="info-box" :color="$store.state.selectedColor" :name="colorList[$store.state.selectedColor].name" :description="colorList[$store.state.selectedColor].description" />
-		<color-list-view class="list-view" :color-list="colorList" />
+		<div class="list-view-wrapper">
+			<color-list-view class="list-view" :color-list="colorList" />
+		</div>
 	</div>
 </template>
 
@@ -17,8 +19,9 @@ export default {
 
 <style>
 *{box-sizing:border-box}
-.app{position:absolute;width:100%;min-height:100%;margin:0;left:0;top:0;overflow-y:visible;overflow-x:auto}
-.app{padding:64px;transition-property:background,color;transition-duration:350ms;min-width:760px}
+.app{position:absolute;width:100%;height:100%;margin:0;left:0;top:0}
+.app{padding:32px;transition-property:background,color;transition-duration:350ms;min-width:720px}
+.list-view-wrapper{position:absolute;width:100%;height:100%;margin:0;left:0;top:0;padding:32px;overflow-y:visible;overflow-x:auto}
 .list-view{width:50%}
-.info-box{width:50%;float:right;position:relative;height:calc(100vh - 128px)}
+.info-box{float:right;position:absolute;left:50%;right:64px;height:calc(100vh - 64px)}
 </style>
--- src/components/colorListItem.vue
+++ src/components/colorListItem.vue
@@ -1,8 +1,8 @@
 <template>
 	<router-link :to="color.slice(1)" class="link">
-		<div class="colorblock" :style="{'background-color':color,'color':textColor}">
-			<h4>{{name}}</h4>
-			<p>{{color}}</p>
+		<div class="colorblock" :style="{'background-color':color,'color':textColor,'border-color':$store.state.selectedColor==color?textColor:'transparent'}">
+			<div class="name">{{name}}</div>
+			<div class="hex">{{color}}</div>
 		</div>
 	</router-link>
 </template>
@@ -23,5 +23,8 @@ export default {
 
 <style scoped>
 .link{color:inherit;text-decoration:none;width:120px;height:120px;margin:8px}
-.colorblock{width:100%;height:100%;padding:8px}
+.colorblock{width:100%;height:100%;padding:8px;position:relative;border:1px solid;transition-property:border,color;transition-duration:350ms}
+.colorblock>div{position:absolute;left:8px;right:8px}
+.name{top:8px;font-weight:bold}
+.hex{bottom:8px}
 </style>
